1e337b432651912ed11615d03775e355f47c5889
Konstantin Mamonov <stanze.von.saidenberg@yandex.ru>
2020-08-02 15:48:10 +0300

    hmr

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,10 +1,31 @@
+import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-export function Header() {
+// function HeaderComponent() {
+//   const [state, setState] = React.useState(false);
+//   return (
+//     <header>
+//       <h1>Hmr example</h1> 
+//       <button onClick={() => setState(!state)}>
+//         {state ? 'hide' : 'show'}
+//       </button>
+//       {state && <h3>Here i'm</h3>}
+//       <p>
+//         It replace code wethout page reloading
+//       </p>
+//     </header>  
+//   );
+// }
+
+function HeaderComponent() {
   return (
     <header>
-      <h1>Reddit for our own 123</h1> 
-    </header>  
+      <h1>Reddit for our own</h1>
+      <p>
+        Hello World
+      </p>
+    </header>      
   );
-
 }
+
+export const Header = hot(HeaderComponent);
